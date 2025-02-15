7170d30e66be83cacf05741fe1b61c4962b0728f
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-15 13:45:36 +0100

    cleaning of code

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,22 +1,26 @@
-import { SubscribeMessage, MessageBody, ConnectedSocket, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  handleConnection(client: any) {
+  handleConnection(client: any) {}
 
-  }
-
-  handleDisconnect(client: any) { 
-
-  }
+  handleDisconnect(client: any) {}
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-   this.server.emit('message', data);
+  handleMessage(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.server.emit('message', data);
   }
 }
